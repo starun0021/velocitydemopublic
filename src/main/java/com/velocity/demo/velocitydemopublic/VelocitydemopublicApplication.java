96b59253ce5288f25df0632dcce8fbe79b576a4b
Tarun Sharma <starun0021@gmail.com>
2022-12-01 18:53:27 +0530

    main - initial commit.

---
+++ src/main/java/com/velocity/demo/velocitydemopublic/VelocitydemopublicApplication.java
@@ -0,0 +1,13 @@
+package com.velocity.demo.velocitydemopublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocitydemopublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelocitydemopublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velocity/demo/velocitydemopublic/VelocitydemopublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velocity.demo.velocitydemopublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelocitydemopublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
